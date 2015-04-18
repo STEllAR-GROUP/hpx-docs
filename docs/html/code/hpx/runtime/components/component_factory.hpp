7e6491590990343b7d6e6d1d19568dac07862707
StellarBot <hpx-devel@stellar.cct.lsu.edu>
2015-04-18 04:02:57 -0500

    Updating docs

--- docs/html/code/hpx/runtime/components/component_factory.hpp
+++ docs/html/code/hpx/runtime/components/component_factory.hpp
@@ -1,11 +1,42 @@
-//  Copyright (c) 2007-2014 Hartmut Kaiser
+//  Copyright (c) 2007-2015 Hartmut Kaiser
 //  Copyright (c) 2011      Bryce Lelbach
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+/// \file hpx/runtime/components/component_factory.hpp
+
 #if !defined(HPX_COMPONENT_FACTORY_SEP_26_2008_0647PM)
 #define HPX_COMPONENT_FACTORY_SEP_26_2008_0647PM
+
+#ifdef DOXYGEN
+/// \def HPX_REGISTER_COMPONENT(type, name, mode)
+///
+/// \brief Define a component factory for a component type
+///
+/// This macro is used create and to register a minimal component factory for
+/// a component type which allows it to be remotely created using the
+/// \a hpx::new_<> function.
+///
+/// This macro can be invoked with one, two or three arguments
+///
+/// \param type The \a type parameter is a (fully decorated) type of the
+///             component type for which a factory should be defined.
+///
+/// \param name The \a name parameter specifies the name to use to register
+///             the factory. This should uniquely (system-wide) identify the
+///             component type. The \a name parameter must conform to the C++
+///             identifier rules (without any namespace).
+///             If this parameter is not given, the first parameter is used.
+///
+/// \param mode The \a mode parameter has to be one of the defined enumeration
+///             values of the enumeration \a hpx::components::factory_state_enum.
+///             The default for this parameter is
+///             \a hpx::components::factory_enabled.
+///
+#define HPX_REGISTER_COMPONENT(type, name, mode)
+
+#else
 
 #include <hpx/config.hpp>
 #include <hpx/hpx_fwd.hpp>
@@ -64,7 +95,7 @@ namespace hpx { namespace components
                 local_settings_ = *local;
         }
 
-        ///
+        /// Destruct a \a component_factory instance
         ~component_factory() {}
 
         /// \brief Return the unique identifier of the component type this
@@ -118,16 +149,6 @@ namespace hpx { namespace components
             return unique_component_name<component_factory>::call();
         }
 
-        /// \brief  The function \a get_factory_properties is used to
-        ///         determine, whether instances of the derived component can
-        ///         be created in blocks (i.e. more than one instance at once).
-        ///         This function is used by the \a distributing_factory to
-        ///         determine a correct allocation strategy
-        factory_property get_factory_properties() const
-        {
-            return Component::get_factory_properties();
-        }
-
         /// \brief Create one or more new component instances.
         ///
         /// \param count  [in] The number of component instances to
@@ -164,7 +185,7 @@ namespace hpx { namespace components
         /// \return   Returns the GID of the first newly created component
         ///           instance.
         naming::gid_type create_with_args(
-            HPX_STD_FUNCTION<void(void*)> const& ctor)
+            util::function_nonser<void(void*)> const& ctor)
         {
             if (isenabled_)
             {
@@ -193,7 +214,7 @@ namespace hpx { namespace components
         ///           instance (this is the same as assign_gid, if successful).
         naming::gid_type create_with_args(
             naming::gid_type const& assign_gid,
-            HPX_STD_FUNCTION<void(void*)> const& ctor)
+            util::function_nonser<void(void*)> const& ctor)
         {
             if (isenabled_)
             {
@@ -259,9 +280,13 @@ namespace hpx { namespace components
 }}
 
 ///////////////////////////////////////////////////////////////////////////////
-/// This macro is used create and to register a minimal component factory with
-/// Hpx.Plugin.
+// This macro is used create and to register a minimal component factory with
+// Hpx.Plugin.
 #define HPX_REGISTER_MINIMAL_COMPONENT_FACTORY(...)                           \
+    HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_(__VA_ARGS__)                      \
+/**/
+
+#define HPX_REGISTER_COMPONENT(...)                                           \
     HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_(__VA_ARGS__)                      \
 /**/
 
@@ -277,7 +302,7 @@ namespace hpx { namespace components
         HPX_DEFINE_GET_COMPONENT_TYPE(ComponentType::wrapped_type)            \
 /**/
 
-
+///////////////////////////////////////////////////////////////////////////////
 #define HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_(...)                          \
     HPX_UTIL_EXPAND_(BOOST_PP_CAT(                                            \
         HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_, HPX_UTIL_PP_NARG(__VA_ARGS__)\
@@ -304,5 +329,59 @@ namespace hpx { namespace components
         ComponentType, componentname, state)                                  \
 /**/
 
+///////////////////////////////////////////////////////////////////////////////
+#define HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC(...)                   \
+    HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_(__VA_ARGS__)              \
+/**/
+
+// same as above, just a better name
+#define HPX_REGISTER_COMPONENT_DYNAMIC(...)                                   \
+    HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_(__VA_ARGS__)              \
+/**/
+
+#define HPX_REGISTER_ENABLED_COMPONENT_FACTORY_DYNAMIC(ComponentType,         \
+            componentname)                                                    \
+        HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_3(                     \
+            ComponentType, componentname, ::hpx::components::factory_enabled) \
+        HPX_DEFINE_GET_COMPONENT_TYPE(ComponentType::wrapped_type)            \
+/**/
+
+#define HPX_REGISTER_DISABLED_COMPONENT_FACTORY_DYNAMIC(ComponentType,        \
+            componentname)                                                    \
+        HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_3(                     \
+            ComponentType, componentname, ::hpx::components::factory_disabled)\
+        HPX_DEFINE_GET_COMPONENT_TYPE(ComponentType::wrapped_type)            \
+/**/
+
+
+#define HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_(...)                  \
+    HPX_UTIL_EXPAND_(BOOST_PP_CAT(                                            \
+        HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_,                      \
+            HPX_UTIL_PP_NARG(__VA_ARGS__)                                     \
+    )(__VA_ARGS__))                                                           \
+/**/
+#define HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_1(ComponentType)       \
+    HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_3(                         \
+        ComponentType, ComponentType, ::hpx::components::factory_check)       \
+    HPX_DEFINE_GET_COMPONENT_TYPE(ComponentType::wrapped_type)                \
+/**/
+#define HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_2(ComponentType,       \
+        componentname)                                                        \
+    HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_3(                         \
+        ComponentType, componentname, ::hpx::components::factory_check)       \
+    HPX_DEFINE_GET_COMPONENT_TYPE(ComponentType::wrapped_type)                \
+/**/
+#define HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC_3(                     \
+        ComponentType, componentname, state)                                  \
+    HPX_REGISTER_COMPONENT_FACTORY_DYNAMIC(                                   \
+        hpx::components::component_factory<ComponentType>, componentname)     \
+    HPX_DEF_UNIQUE_COMPONENT_NAME(                                            \
+        hpx::components::component_factory<ComponentType>, componentname)     \
+    template struct hpx::components::component_factory<ComponentType>;        \
+    HPX_REGISTER_MINIMAL_COMPONENT_REGISTRY_DYNAMIC_3(                        \
+        ComponentType, componentname, state)                                  \
+/**/
+
 #endif
 
+#endif

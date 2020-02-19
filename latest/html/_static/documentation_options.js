499aa77ceee36e49ea223e81d0bef53b69078d5b
Mikael Simberg <mikael.simberg@iki.fi>
2020-02-19 14:19:28 +0100

    Update latest documentation

--- latest/html/_static/documentation_options.js
+++ latest/html/_static/documentation_options.js
@@ -3,9 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '1.4.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    SOURCELINK_SUFFIX: '.txt'
 };
